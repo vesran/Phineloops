4b2a75a1ad016c31be0ace5b3e4f3340b2475c6a
KarimAmrouche <m.amr@outlook.fr>
2019-11-21 19:01:03 +0100

    first_version

--- src/main/java/model/Piece.java
+++ src/main/java/model/Piece.java
@@ -1,5 +1,38 @@
 package model;
 
-public class Piece {
+import java.util.HashMap;
+
+public abstract class Piece {
+	protected HashMap<Orientation, Piece> neighbor;
+	protected int id;
+	protected int orientation;
+
+	public HashMap<Orientation, Piece> getNear() {
+		return neighbor;
+	}
+
+//	public void setNear(HashMap<Orientation, Piece> neighbor) { pas encore finie
+//		this.neighbor = neighbor;
+//	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getOrientation() {
+		return orientation;
+	}
+
+	public void setOrientation(int orientation) {
+		this.orientation = orientation;
+	}
+
+	public abstract void translation_right();
+
+	public abstract void translation_left();
 
 }
