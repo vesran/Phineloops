34bad7dcdc4accdfe4afd9568f94bf953dc8904d
vesran <tran.yves@gmail.com>
2019-11-24 13:46:24 +0100

    can display a window

--- src/main/java/view/PhineLoopsMainGUI.java
+++ src/main/java/view/PhineLoopsMainGUI.java
@@ -1,4 +1,24 @@
 package view;
 
-public class PhineLoopsMainGUI {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PhineLoopsMainGUI extends Application {
+
+    static final int WIDTH = 500;
+    static final int HEIGHT = 500;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+
+
+
+        stage.setTitle("Phine Loops Game");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
