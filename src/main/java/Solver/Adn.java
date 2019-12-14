f703cf281d620ed5a3166fb393481c78fbdc517b
Bilal KHALDI <bilal.khaldi@dauphine.eu>
2019-12-14 21:55:26 +0100

    Create class Adn

--- src/main/java/Solver/Adn.java
+++ src/main/java/Solver/Adn.java
@@ -1,4 +1,16 @@
 package Solver;
 
+import model.Piece;
+
 public class Adn {
+	private Piece[][] m_grid ;
+	
+	public Adn(Piece[][] myAdn) {
+		this.m_grid = myAdn ;
+	}
+	
+	public void mutation() {
+		
+	}
+	
 }
