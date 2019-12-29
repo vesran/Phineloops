af9e72466f46f76dd972fdeab8ad2fd78d76198a
Bilal KHALDI <bilal.khaldi@dauphine.eu>
2019-12-29 18:04:32 +0100

    Adding comment Solving interface

--- src/main/java/Solver/Solving.java
+++ src/main/java/Solver/Solving.java
@@ -1,5 +1,10 @@
 package Solver;
-
+/**
+ *  @author Karim Amrouche
+ * @author Bilal Khaldi
+ * @author Yves Tran
+ * Interface to use for implements a Solver
+ */
 public interface Solving {
 	public boolean solving(Extend extend) ;
 }
