b8ce1e911cc899dd2f941c079e2034f8b1e225a8
KarimAmrouche <m.amr@outlook.fr>
2019-12-29 18:24:43 +0100

    checkAndGenerate

--- src/main/java/fr/dauphine/javaavance/phineloops/Main.java
+++ src/main/java/fr/dauphine/javaavance/phineloops/Main.java
@@ -4,6 +4,7 @@ import Solver.Csp;
 import Solver.Extend;
 import Solver.quasiexhaustive.QuasiExhaustiveSolver;
 import Solver.quasiexhaustive.comparaison.Lexicographic;
+import model.First_Generator;
 import model.Level;
 import model.io.FileCreator;
 import model.io.FileReader;
@@ -23,9 +24,11 @@ public class Main {
 	private static Integer height = -1;
 	private static Integer maxcc = -1;
 
-	private static void generate(int width, int height, String outputFile) {
+	private static void generate(int width, int height, String outputFile) throws IllegalArgumentException {
 		// generate grid and store it to outputFile...
 		// ...
+		First_Generator level = new First_Generator(width, height, 2);
+		FileCreator.write(level.getL(), outputFile);
 	}
 
 	private static boolean solve(String inputFile, String outputFile) {
@@ -66,6 +69,7 @@ public class Main {
 		options.addOption("G", "gui", true, "Run with the graphic user interface.");
 		options.addOption("GS", "guisolver", true, "Run with the graphic interface showing an exhaustive solver working.");
 		options.addOption("h", "help", false, "Display this help");
+		
 		try {
 			cmd = parser.parse(options, args);
 		} catch (ParseException e) {
